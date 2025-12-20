b2e4d00e0bae6a147c583107d93cc1630b29d111
ANVIKA MADDULA <anvikamaddula6677@gmail.com>
2025-12-20 16:55:48 +0530

    Initial Spring Boot backend setup

---
+++ backend-springboot/chatbot-backend/src/main/java/com/college/ragbackend/RagbackendApplication.java
@@ -0,0 +1,13 @@
+package com.college.ragbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagbackendApplication.class, args);
+	}
+
+}
---
+++ backend-springboot/chatbot-backend/src/test/java/com/college/ragbackend/RagbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.ragbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
